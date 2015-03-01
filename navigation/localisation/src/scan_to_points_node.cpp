fe3e36c04ab9f809b27173d6a21d609535517ce2
tdanel <tdanel@tdanel-VirtualBox.(none)>
2015-03-01 16:07:18 +0100

    modifs des fonctions de conversion pour publication via topics

--- navigation/localisation/src/scan_to_points_node.cpp
+++ navigation/localisation/src/scan_to_points_node.cpp
@@ -2,22 +2,9 @@
 #include "laserScan.h"
 #include "deplacement_msg/Points.h"
 #include "sensor_msgs/LaserScan.h"
+#include "line_detection_utils.h"
 #include <vector>
 #include <cmath>
-
-std::vector<deplacement_msg::Point> convertPtToDeplMsgPt(const std::vector<Point> &points){
-    std::vector<deplacement_msg::Point> punkt;
-    punkt.resize(points.size());
-    for(auto p:points)
-    {
-        deplacement_msg::Point pt;
-        pt.x=p.x;
-        pt.y=p.y;
-        punkt.push_back(pt);
-    }
-
-    return punkt;
-}
 
 int main(int argc, char** argv)
 {
@@ -32,14 +19,14 @@ int main(int argc, char** argv)
 
     ros::Subscriber sub_laser  = n.subscribe("/scan", 1000, &laserScan::laserCallback, &laserData);
 
-    ros::Publisher Points_pub = n.advertise<deplacement_msg::Points>("/points", 1000);
+    ros::Publisher points_pub = n.advertise<deplacement_msg::Points>("/points", 1000);
 
     ros::Rate loop_rate (100);
 
     while(n.ok())
     {
         deplacement_msg::Points msgPoints;
-        msgPoints.points=convertPtToDeplMsgPt(laserData.getPoints());
+        msgPoints = convertPtsToDeplMsgPts(laserData.getPoints());
 
         // Publish
         Points_pub.publish(msgPoints);
