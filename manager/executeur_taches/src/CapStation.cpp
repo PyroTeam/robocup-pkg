aef1481613c6eea339039d291574eaafcd6d5972
sandra <sandra@sandra-VirtualBox.(none)>
2015-03-18 17:24:26 +0100

    Emetteur Topic et quelques modifications

--- manager/executeur_taches/src/CapStation.cpp
+++ manager/executeur_taches/src/CapStation.cpp
@@ -7,6 +7,10 @@ CapStation::CapStation(){
   m_type = "CapStation";
   m_capNoir = 0;
   m_capGris = 0;
+  m_stock[0] = 0; m_stock[1] = 0; m_stock[2] = 0;
+  m_stockage.x = 0.0;
+  m_stockage.y = 0.0;
+  m_stockage.theta = 0.0; 
 }
 
 /* Destructeur */
@@ -28,3 +32,13 @@ void CapStation::majNoir(int nbNoir){
 void CapStation::majGris(int nbGris){
   m_capGris = nbGris;
 }
+
+//int CapStation::stocker(){
+	/* MANQUE DU CODE */
+/*	AllerMachine(m_stockage);
+	int i = 1;
+	while(i <= m_stock.length && !m_stock[i]){
+		i++;
+	} 
+	return i;
+}*/
