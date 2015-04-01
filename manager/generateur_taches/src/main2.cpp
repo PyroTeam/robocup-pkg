ce5670e147fb5a119b767a9a9e0817e7642457a6
cyrilsmg <smg.cyril@gmail.com>
2015-04-01 16:52:45 +0200

    rectificatif envoi ordre avec stockage

--- manager/generateur_taches/src/main2.cpp
+++ manager/generateur_taches/src/main2.cpp
@@ -10,14 +10,11 @@
 #include "travail.h"
 #include "robot.h"
 #include "produit.h"
-#include "srvorder.h"
-#include "tableaustockage.h"
 #include "machine.h"
-#include "tabmachine.h"
 #include "travailphase.h"
+#include "etatdujeu.h"
 
-#include "manager_msg/order.h"
-#include "manager_msg/activity.h"
+#include "manager_msg/GameState.h" //a changer en comm_msg plus tard
 
 using namespace std;
 using namespace manager_msg;
@@ -28,9 +25,10 @@ int main(int argc, char **argv) {
   
   ros::init(argc, argv, "action_node");
   Action action_exec;
+  Etatdujeu etat_jeu;
   ros::Rate loop_rate(1);
   
-  int temps = 5,cap_dispo = 0,storage =0, id=0, cpt_order = 0, k=0; 
+  int cap_dispo = 0,storage =0, id=0, cpt_order = 0, k=0; 
   Stockage tab_stock[6];
   
   Robot tabrobot[3];
@@ -45,14 +43,17 @@ int main(int argc, char **argv) {
   cout <<""<<endl;
   
   vector<int> couleurs;
-  couleurs.push_back(0);
-  couleurs.push_back(1);
+  couleurs.push_back(10);
+  couleurs.push_back(20);
   Produit product(0,couleurs);
-  Ordre order(product,100,150,2,false);
+  Ordre order(product,20,30,2,false);
   
   Machine tab_machine[6];
   
+  bool take[3] = {false,false,false};
   
+  double t0 = ros::Time::now().toSec();
+  double temps = t0;
   
   /***FONCTION PRINCIPALE ***/
     
@@ -60,17 +61,19 @@ int main(int argc, char **argv) {
   while(ros::ok() && !work.empty()) {
     // il y a trois robots
     for(int j=0; j<3; j++){
+      temps = ros::Time::now().toSec() - t0;
+      cout << "temps en sec = " << temps << endl;
       action_exec.update_robot(tabrobot);
       //mettre a jour les infos envoyees par la refbox
-      
-      if(1/* etat du jeu=phase d exploration*/    && !exploration_finie(tab_machine)){
-        travail_phase_exploration(tab_machine,tabrobot,cpt_order,j);  
+      if(!tabrobot[j].get_occupe()){
+        if(etat_jeu.get_phase()==GameState::EXPLORATION && !exploration_finie(tab_machine)){
+          travail_phase_exploration(tab_machine,tabrobot,cpt_order,j);  
+        }
+        if(etat_jeu.get_phase()==GameState::PRODUCTION && !work.empty()){
+          travail_phase_production(work,tab_machine,tabrobot,tab_stock,take,cpt_order,j,cap_dispo,storage,order,temps);
+          get_info_liste_de_liste(work);
+        }
       }
-      
-      if(0/* etat du jeu=phase de production*/    && !work.empty()){
-        travail_phase_production(work,tab_machine,tabrobot,tab_stock,cpt_order,j,temps,cap_dispo,storage,order);
-      }
-    
     ros::spinOnce();
     loop_rate.sleep();
     }
