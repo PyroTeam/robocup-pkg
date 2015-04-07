c4d531aa5cbb998e5b4ce50833f203e0ecafe516
cyrilsmg <smg.cyril@gmail.com>
2015-04-07 18:27:23 +0200

    serveur action fait

--- navigation/approche_finale/src/finalApproaching_server.cpp
+++ navigation/approche_finale/src/finalApproaching_server.cpp
@@ -30,14 +30,59 @@ public:
   void executeCB(const manager_msg::finalApproachingGoalConstPtr &goal)
   {
     // helper variables
-    ros::Rate r(1);
+    ros::Rate loop_rate(1);
     bool success = true;
 
     feedback_.percent_complete = 0;
 
     // publish info to the console for the user
     ROS_INFO("%s: Executing, creating finalApproaching sequence of type %i with side %i and parameter %i", action_name_.c_str(), goal->type, goal->side,goal->parameter);
-
+    int type, side, parameter; 
+    nh_.param<int>("fa_type",type,goal->type);
+    nh_.param<int>("fa_side",side,goal->side);
+    nh_.param<int>("fa_parameter",parameter,goal->parameter);
+    
+    /*
+    bool objectif=false
+    while(objectif==false){
+        while(deplacement_msg::landmarks[2] > 1.60 || geometry_msgs/Pose2D[2] < 1.54){
+           if(deplacement_msg::landmarks[2] > 1.60){
+              //tourner dans le sens anti trigo   
+           }
+           if(deplacement_msg::landmarks[2] < 1.54){
+              //tourner dans le sens trigo  
+           }
+        }
+        percent_complete = 20;
+        
+        if(ARTAG.cote != side){
+           success = false;
+           break;
+        }
+        
+        percent_complete = 40;
+        
+        while(deplacement_msg::landmarks[1] > 0.20){ //distance en metre
+             //avancer vers machine seulement suivant l'axe y   
+        }
+        percent_complete = 60;
+        
+        while(deplacement_msg::landmarks[0]){
+                if(position(message scan,position robot)==1)
+                        aller a droite
+                if(position(message scan,position robot)==2)
+                        aller a gauche
+        }
+        percent_complete = 80;
+        
+        while(distance(laser,machine) > 0.05){
+             avancer vers machine   
+        }
+        percent_complete = 100;
+        objectif=true;   
+    }
+    */
+   
     // start executing the action
     for(int i=1; i<=100; i++)
     {
@@ -51,11 +96,14 @@ public:
         break;
       }
       feedback_.percent_complete++;
+      //tasks to do 
       // publish the feedback
       as_.publishFeedback(feedback_);
-      // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-      r.sleep();
+      
+      
+      loop_rate.sleep();
     }
+    
 
     if(success)
     {
@@ -73,8 +121,10 @@ public:
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "finalApproaching");
+  ROS_INFO("Initialisation du serveur");
 
   finalApproachingAction finalApproaching(ros::this_node::getName());
+  ROS_INFO("test");
   ros::spin();
 
   return 0;
