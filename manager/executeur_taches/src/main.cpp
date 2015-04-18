38846ad5bef2ade8f321d75452427cdeddfd64f2
sandra <sandra@sandra-VirtualBox.(none)>
2015-04-18 14:06:55 +0200

    modification du main + test du code fonctionne

--- manager/executeur_taches/src/main.cpp
+++ manager/executeur_taches/src/main.cpp
@@ -20,26 +20,26 @@ int main(int argc, char **argv) {
     int nb_robot;
     n.param<int>("robotNumber",nb_robot,0);  // nb robot, par défaut 0
     ros::Rate loop_rate(1);
+
+    /* service reponse au générateur de taches */
+    GtServerSrv gtsrv;
+    gtsrv.setId(1);
+    ros::ServiceServer service = n.advertiseService("order", &GtServerSrv::responseToGT, &gtsrv);
+    
+    /* Publisher topic générateur de taches */
+    ros::Publisher activite_pub = n.advertise<manager_msg::activity>("/task_exec_state", 1000);
+
+    /* Subscriber topic localisation */    
+    ros::Subscriber sub = n.subscribe("/landmarks",1000,&LocaSubscriber::tesCallback, &loca_sub);
+
     
     while(ros::ok){
-            /* service reponse au générateur de taches */
-            GtServerSrv gtsrv;
-            ROS_INFO("I'm READY ! ");
-            gtsrv.setId(1);
-            ros::ServiceServer service = n.advertiseService("order", &GtServerSrv::responseToGT, &gtsrv);
-            
-         
-            /* Publisher topic générateur de taches */
-            ros::Publisher activite_pub = n.advertise<manager_msg::activity>("/task_exec_state", 1000);
-            activite_pub.publish(gtsrv.getActivityMsg()); 
+        ROS_INFO("I'm READY ! ");
+        activite_pub.publish(gtsrv.getActivityMsg()); 
 
-            /* Subscriber topic localisation */
-            
-            ros::Subscriber sub = n.subscribe("/landmarks",1000,&LocaSubscriber::tesCallback, &loca_sub);
-
-            /* Let's Spin until the end of the world !! */
-            ros::spinOnce();
-            loop_rate.sleep();
+        /* Let's Spin until the end of the world !! */
+        ros::spinOnce();
+        loop_rate.sleep();
 
     }
 
