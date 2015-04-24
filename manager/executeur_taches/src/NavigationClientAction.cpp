d6db100d67fecfeff024c309d7da99df5c3953a8
vcoelen <vincent.coelen@polytech-lille.net>
2015-04-24 23:15:56 +0200

    Communication entre executeur de tache et navigation :D

--- manager/executeur_taches/src/NavigationClientAction.cpp
+++ manager/executeur_taches/src/NavigationClientAction.cpp
@@ -1,22 +1,22 @@
 #include "NavigationClientAction.h"
 
-NavigationClientAction::NavigationClientAction() { 
+NavigationClientAction::NavigationClientAction() {
 }
 NavigationClientAction::~NavigationClientAction(){}
 
 int NavigationClientAction::goToAPoint(geometry_msgs::Pose2D dest_point){
 
-	actionlib::SimpleActionClient<manager_msg::MoveToPoseAction> client("MoveToPose",true);
+	actionlib::SimpleActionClient<deplacement_msg::MoveToPoseAction> client("moveToPose",true);
 
 	ROS_INFO("Waiting for action Navigation Server to start");
 
-	client.waitForServer(); 
+	client.waitForServer();
 
 	ROS_INFO("Action server started, sending goal");
 
-	manager_msg::MoveToPoseGoal goal;
+	deplacement_msg::MoveToPoseGoal goal;
 	goal.position_finale = dest_point;
-	client.sendGoal(goal);  
+	client.sendGoal(goal);
 
 	//wait for the action to return
 	bool finished_before_timeout = client.waitForResult(ros::Duration(5.0));
