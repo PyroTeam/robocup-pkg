b5050b2a50adfcd5d33b993847bee88d60fd85d1
Valentin VERGEZ <ValentinVERGEZ@users.noreply.github.com>
2015-04-25 12:15:00 +0200

    Grid_Maker ATTENTION : 	* Dessin en dur de zones interdites pour phase d'explo de deliveryMachine statique 	* Dessin en dur de la DeliveryMachine dans contrusct_map_test

--- navigation/grid_maker/src/Test.cpp
+++ navigation/grid_maker/src/Test.cpp
@@ -15,72 +15,72 @@ int main(int argc, char **argv)
         deplacement_msg::Landmarks machines_msgs;
         geometry_msgs::Pose2D tab;
         // - Left Side
-        // Zone 21
-        tab.x=-5;
-        tab.y=1;
-        tab.theta=0;
-        machines_msgs.landmarks.push_back(tab);
-        // Zone 23
-        tab.x=-4.5;
-        tab.y=3+0.6;
-        tab.theta=0;
-        machines_msgs.landmarks.push_back(tab);
-        // Zone 24
-        tab.x=-4.85;
-        tab.y=4.5+0.4;
-        tab.theta=-M_PI/4;
-        machines_msgs.landmarks.push_back(tab);
+        // // Zone 21
+        // tab.x=-5;
+        // tab.y=1;
+        // tab.theta=0;
+        // machines_msgs.landmarks.push_back(tab);
+        // // Zone 23
+        // tab.x=-4.5;
+        // tab.y=3+0.6;
+        // tab.theta=0;
+        // machines_msgs.landmarks.push_back(tab);
+        // // Zone 24
+        // tab.x=-4.85;
+        // tab.y=4.5+0.4;
+        // tab.theta=-M_PI/4;
+        // machines_msgs.landmarks.push_back(tab);
 
-        // Zone 18
-        tab.x=-3.5;
-        tab.y=1.5+0.75;
-        tab.theta=M_PI/2;
-        machines_msgs.landmarks.push_back(tab);
+        // // Zone 18
+        // tab.x=-3.5;
+        // tab.y=1.5+0.75;
+        // tab.theta=M_PI/2;
+        // machines_msgs.landmarks.push_back(tab);
 
-        // Zone 14
-        tab.x=-1;
-        tab.y=1.5+0.75;
-        tab.theta=-M_PI/4;
-        machines_msgs.landmarks.push_back(tab);
+        // // Zone 14
+        // tab.x=-1;
+        // tab.y=1.5+0.75;
+        // tab.theta=-M_PI/4;
+        // machines_msgs.landmarks.push_back(tab);
         // Zone 16
-        tab.x=-1;
-        tab.y=3+0.75;
+        tab.x=-0.55;
+        tab.y=5.35;
         tab.theta=M_PI/2;
         machines_msgs.landmarks.push_back(tab);
 
 
         // - Right Side
-        // Zone 21
-        tab.x=5;
-        tab.y=1;
-        tab.theta=0;
-        machines_msgs.landmarks.push_back(tab);
-        // Zone 23
-        tab.x=4.5;
-        tab.y=3+0.6;
-        tab.theta=0;
-        machines_msgs.landmarks.push_back(tab);
-        // Zone 24
-        tab.x=4.85;
-        tab.y=4.5+0.4;
-        tab.theta=+M_PI/4;
-        machines_msgs.landmarks.push_back(tab);
+        // // Zone 21
+        // tab.x=5;
+        // tab.y=1;
+        // tab.theta=0;
+        // machines_msgs.landmarks.push_back(tab);
+        // // Zone 23
+        // tab.x=4.5;
+        // tab.y=3+0.6;
+        // tab.theta=0;
+        // machines_msgs.landmarks.push_back(tab);
+        // // Zone 24
+        // tab.x=4.85;
+        // tab.y=4.5+0.4;
+        // tab.theta=+M_PI/4;
+        // machines_msgs.landmarks.push_back(tab);
 
-        // Zone 18
-        tab.x=3.5;
-        tab.y=1.5+0.75;
-        tab.theta=-M_PI/2;
-        machines_msgs.landmarks.push_back(tab);
+        // // Zone 18
+        // tab.x=3.5;
+        // tab.y=1.5+0.75;
+        // tab.theta=-M_PI/2;
+        // machines_msgs.landmarks.push_back(tab);
 
-        // Zone 14
-        tab.x=1;
-        tab.y=1.5+0.75;
-        tab.theta=+M_PI/4;
-        machines_msgs.landmarks.push_back(tab);
+        // // Zone 14
+        // tab.x=1;
+        // tab.y=1.5+0.75;
+        // tab.theta=+M_PI/4;
+        // machines_msgs.landmarks.push_back(tab);
         // Zone 16
-        tab.x=1;
-        tab.y=3+0.75;
-        tab.theta=-M_PI/2;
+        tab.x=0.55;
+        tab.y=5.35;
+        tab.theta=M_PI/2;
         machines_msgs.landmarks.push_back(tab);
 
         Pose_Machines_Pub.publish(machines_msgs);
