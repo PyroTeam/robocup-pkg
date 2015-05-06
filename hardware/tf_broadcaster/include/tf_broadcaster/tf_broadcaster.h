ecc4543370c9129dc12fbaa884de882ae7aaeb25
ValentinVERGEZ <valentin.vergez@gmail.com>
2015-05-06 08:02:50 +0200

    Simulateur, premiere version : 	* Ajout d'un tf_broadcaster 	* Modification du gazebo_to_ros pour avoir les vitesses sur odom et travailler avec les bons reperes 	* Ajout d'un pkg contenant les launchfiles et les worlds pour lancer la simulation gazebo

---
+++ hardware/tf_broadcaster/include/tf_broadcaster/tf_broadcaster.h
@@ -0,0 +1,13 @@
+#ifndef _TF_BROADCASTER__TF_BROADCASTER_H_
+#define _TF_BROADCASTER__TF_BROADCASTER_H_
+
+/*==========  Includes  ==========*/
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include "nav_msgs/Odometry.h"
+
+/*==========  Prototypes  ==========*/
+void poseCallback(const nav_msgs::Odometry& odom);
+
+#endif // _TF_BROADCASTER__TF_BROADCASTER_H_
