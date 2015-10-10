4d9fc83953f6e84038081d7b385ceb1eb356f9f3
robotino <elise.tissot@polytech-lille.net>
2015-10-10 13:41:22 +0200

    Modifs pour tests évitement situation réelle

--- navigation/path_tracker/src/dataLaser.cpp
+++ navigation/path_tracker/src/dataLaser.cpp
@@ -41,6 +41,7 @@ void DataLaser::recoverDataLaser()
     if (ranges.size() != 0)
     {
         m_dataLaser.clear();
+	m_listener.waitForTransform("/odom", m_scan.header.frame_id, ros::Time(0), ros::Duration(1));
         m_listener.lookupTransform("/odom", m_scan.header.frame_id, ros::Time(0), m_transform);
         //ROS_INFO("Position robot : x = %f, y = %f", m_transform.getOrigin().x(), m_transform.getOrigin().y()); 
         for (int i = 0 ; i < ranges.size() ; i++)
