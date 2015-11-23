36c3943dc0f7227835f6526fdb7162227a0d18cf
vcoelen <vincent.coelen@polytech-lille.net>
2015-11-23 15:49:10 +0100

    Modification de GridMapGraph.cpp non enregistrée au moment du précédent commit.

--- navigation/path_finder/src/graph/GridMapGraph.cpp
+++ navigation/path_finder/src/graph/GridMapGraph.cpp
@@ -32,18 +32,6 @@ void GridMapGraph::setGridMap(const nav_msgs::OccupancyGrid &grid)
     m_grid = grid;
 }
 
-void GridMapGraph::search(std::shared_ptr<State> &startState, std::shared_ptr<State> &endState, std::list<std::shared_ptr<State>> &path)
-{
-    if (m_isInit)
-    {
-        m_searchAlgo->search(startState, endState, path);
-    }
-    else
-    {
-        ROS_ERROR("Tentative de recherche de trajectoire, alors qu'aucune map n'a été reçue.");
-    }
-}
-
 void GridMapGraph::getSuccessors(const std::shared_ptr<State> &state, std::list<std::shared_ptr<State>> &succ)
 {
     succ.clear();
