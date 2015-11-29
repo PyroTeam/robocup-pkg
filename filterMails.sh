8b6dd8beab9a64f4ee6ab2f70559b085d043cf14
ValentinVERGEZ <valentin.vergez@gmail.com>
2015-11-29 23:44:53 +0100

    Correction du script filterMails.sh

--- filterMails.sh
+++ filterMails.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 ## Only does it when stricly aware of what you are doing
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}" )" && pwd )/"
 if [ "$1" != "-f" ];then
 	echo "mkdir ~/tmp"
 	echo "cd ~/tmp"
 	echo "git clone --bare https://github.com/PyroTeam/robocup-pkg.git"
 	echo "cd robocup-pkg.git"
 
+	echo ""
+	echo "$SCRIPT_DIR/filterMails.sh"
 	exit 666
 fi
 
-./cleanScripts/cleanMails_Vincent2.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Vincent2.sh
 rm -rf refs/original/
-cleanScripts/cleanMails_Vincent.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Vincent.sh
 rm -rf refs/original/
-./cleanScripts/cleanMails_Sandra.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Sandra.sh
 rm -rf refs/original/
-./cleanScripts/cleanMails_Thomas.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Thomas.sh
 rm -rf refs/original/
-./cleanScripts/cleanMails_Romain.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Romain.sh
 rm -rf refs/original/
-./cleanScripts/cleanMails_Valentin.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Valentin.sh
 rm -rf refs/original/
-./cleanScripts/cleanMails_Elise.sh
+$SCRIPT_DIR/cleanScripts/cleanMails_Elise.sh
 rm -rf refs/original/
 
 echo ""
