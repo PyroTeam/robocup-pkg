5769b8d9d3e24d38dc6f928fe0afd75a9aa1e8fc
SandraHage <shage93@gmail.com>
2016-06-28 15:25:06 +0200

    premier commit

--- manager/executeur_taches/src/GtServerSrv.cpp
+++ manager/executeur_taches/src/GtServerSrv.cpp
@@ -348,6 +348,7 @@ bool GtServerSrv::responseToGT(manager_msg::order::Request &req,manager_msg::ord
       {
         ROS_ERROR("ERROR: req.id is not between 0 and 5 ");
         res.accepted =false;
+        res.needToResendOrder = true;
       }
       break;
 
@@ -442,6 +443,7 @@ bool GtServerSrv::responseToGT(manager_msg::order::Request &req,manager_msg::ord
         {
           ROS_ERROR("Unable to get correct AR Tag from this machine. Abort service");
           res.accepted = false;
+          res.noProblem = false;
           break;
         }
 
@@ -468,6 +470,7 @@ bool GtServerSrv::responseToGT(manager_msg::order::Request &req,manager_msg::ord
             // TODO: abandonner le service
             ROS_ERROR("Unable to reach output for this MPS. Abort service");
             res.accepted = false;
+            res.noProblem = false;
             break;
           }
         }
