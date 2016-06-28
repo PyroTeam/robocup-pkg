08ea9b98663f0c7e87043b0f367a99f0244c936d
tdanel <th.danel@gmail.com>
2016-06-28 15:33:21 +0200

    Recup avant pull

--- manager/executeur_taches/src/GtServerSrv.cpp
+++ manager/executeur_taches/src/GtServerSrv.cpp
@@ -348,7 +348,6 @@ bool GtServerSrv::responseToGT(manager_msg::order::Request &req,manager_msg::ord
       {
         ROS_ERROR("ERROR: req.id is not between 0 and 5 ");
         res.accepted =false;
-        res.needToResendOrder = true;
       }
       break;
 
@@ -443,7 +442,6 @@ bool GtServerSrv::responseToGT(manager_msg::order::Request &req,manager_msg::ord
         {
           ROS_ERROR("Unable to get correct AR Tag from this machine. Abort service");
           res.accepted = false;
-          res.noProblem = false;
           break;
         }
 
@@ -470,7 +468,6 @@ bool GtServerSrv::responseToGT(manager_msg::order::Request &req,manager_msg::ord
             // TODO: abandonner le service
             ROS_ERROR("Unable to reach output for this MPS. Abort service");
             res.accepted = false;
-            res.noProblem = false;
             break;
           }
         }
