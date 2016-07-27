97d4f95fa7dcf75b91bcf74ec53e7fcf12e31444
tdanel <th.danel@gmail.com>
2016-07-27 13:43:52 +0200

    Fix merge foiré + compilation

--- manager/executeur_taches/src/GripperClientSrv.cpp
+++ manager/executeur_taches/src/GripperClientSrv.cpp
@@ -13,8 +13,8 @@ GripperClientSrv::~GripperClientSrv()
 bool GripperClientSrv::gripper_update(bool new_state)
 {
 	ros::NodeHandle n;
-	ros::ServiceClient client = n.serviceClient<manager_msg::SetGripper>("fakeRobotino/setGripper");
-	manager_msg::SetGripper srv;
+	ros::ServiceClient client = n.serviceClient<gripper_msg::SetGripper>("fakeRobotino/setGripper");
+	gripper_msg::SetGripper srv;
 	srv.request.state = new_state;
 	if (client.call(srv))
 	{
